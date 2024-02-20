76af1431b7801f83272aca2a44e6189ee7d7e7a1
TusharKhari <tusharkhari1@gmail.com>
2024-02-20 14:22:46 +0530

    banner

--- lib/features/home/widget/home_offer_banner_widget.dart
+++ lib/features/home/widget/home_offer_banner_widget.dart
@@ -1,120 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:new_user_side/resources/common/buttons/my_buttons.dart';
-import 'package:new_user_side/resources/common/my_text.dart';
-import 'package:new_user_side/utils/constants/app_colors.dart';
+ import 'package:new_user_side/utils/constants/app_colors.dart';
 import 'package:new_user_side/utils/extensions/extensions.dart';
 
 import '../../../resources/font_size/font_size.dart';
 import '../../estimate/screens/estimate_generation_screen.dart';
 
-class HomeOfferBannerOld extends StatelessWidget {
-  const HomeOfferBannerOld({
-    Key? key,
-  }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
-    BorderRadius borderRadius = BorderRadius.circular(24.r);
-    final size = MediaQuery.of(context).size;
-    Border border = Border.all(
-      width: 1.2,
-      color: AppColors.grey.withOpacity(0.2),
-    );
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20.w),
-      width: double.infinity,
-      height: height / 4.46,
-      decoration: BoxDecoration(
-        borderRadius: borderRadius,
-        border: border,
-      ),
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 5.w, vertical: 5.h),
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: borderRadius,
-          border: border,
-          image: const DecorationImage(
-            image: AssetImage("assets/images/room/house.png"),
-            fit: BoxFit.fitWidth,
-          ),
-        ),
-        child: Row(
-          children: [
-            20.hs,
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                height > 800 ? 20.vs : 25.vs,
-                MyTextPoppines(
-                  text: "Get 20% OFF",
-                  // fontSize:
-                  // height > 800
-                  // ? height / MyFontSize.font20
-                  // : height / MyFontSize.font24,
-                  fontSize: 22.sp,
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.white,
-                ),
-                15.vs,
-                SizedBox(
-                  width: 200.w,
-                  child: MyTextPoppines(
-                    text: "Tap on banner to see more in details.",
-                    fontSize: size.height * FontSize.twelve,
-                    //fontSize:
-                    //  height > 800
-                    //     ? height / MyFontSize.font10
-                    //     : height / MyFontSize.font14,
-                    fontWeight: FontWeight.w500,
-                    color: AppColors.white,
-                    height: 1.2.h,
-                  ),
-                ),
-                15.vs,
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding:
-                        EdgeInsets.symmetric(vertical: 8.h, horizontal: 20.w),
-                    decoration: BoxDecoration(
-                      borderRadius: borderRadius,
-                      border: Border.all(
-                        width: 1.2.w,
-                        color: AppColors.white,
-                      ),
-                      color: AppColors.buttonBlue,
-                    ),
-                    child: Center(
-                      child: MyTextPoppines(
-                        text: "View Details",
-                        fontSize: size.height * FontSize.fourteen,
-                        // fontSize: height / MyFontSize.font12,
-                        fontWeight: FontWeight.w500,
-                        color: AppColors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: double.infinity,
-              width: 110.w,
-              child: Image.asset(
-                "assets/images/workers/worker_1.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
 // ========
 
@@ -147,16 +40,19 @@ class _HomeOfferBannerState extends State<HomeOfferBanner> {
         ),
       ),
       child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 5.w, vertical: 5.h),
+        // margin: EdgeInsets.symmetric(horizontal: 5.w, vertical: 5.h),
+        margin: EdgeInsets.all(3.w),
+        padding: EdgeInsets.only(left: 10.w),
         width: double.infinity,
         height: double.infinity,
         decoration: BoxDecoration(
+          color: Color(0xff001D57),
           borderRadius: borderRadius,
           border: border,
-          image: const DecorationImage(
-            image: AssetImage("assets/images/room/house.png"),
-            fit: BoxFit.fitWidth,
-          ),
+          // image: const DecorationImage(
+          //   image: AssetImage("assets/images/room/house.png"),
+          //   fit: BoxFit.fitWidth,
+          // ),
         ),
         child: Row(
           children: [
@@ -166,11 +62,19 @@ class _HomeOfferBannerState extends State<HomeOfferBanner> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "Get an instant quote",
+                  "Free instant quote",
                   style: TextStyle(
                     fontSize: 20.sp,
                     fontWeight: FontWeight.w600,
                     color: Colors.white,
+                  ),
+                ),
+                Text(
+                  "NO OBLIGATION",
+                  style: TextStyle(
+                    fontSize: 13.sp,
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.golden,
                   ),
                 ),
                 MyBlueButton(
@@ -217,11 +121,11 @@ class _HomeOfferBannerState extends State<HomeOfferBanner> {
                 )
               ],
             ),
-            Image.asset(
-              "assets/images/workers/worker_1.png",
-              fit: BoxFit.cover,
-              width: size.width * 0.26,
-            ),
+            // Image.asset(
+            //   "assets/images/workers/worker_1.png",
+            //   fit: BoxFit.cover,
+            //   width: size.width * 0.26,
+            // ),
           ],
         ),
       ),
