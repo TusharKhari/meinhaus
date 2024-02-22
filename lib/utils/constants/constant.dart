a56448c3a6fb1ece2e2984b830a00f1fb2f402a8
TusharKhari <tusharkhari1@gmail.com>
2024-02-22 13:50:01 +0530

    prod

--- lib/utils/constants/constant.dart
+++ lib/utils/constants/constant.dart
@@ -6,16 +6,16 @@ final bool isTest = false;
 
 // test env
 
-final String baseUrl = "https://test.meinhaus.ca/api";
-final String baseUrl2 = "https://test.meinhaus.ca";
-final String stripePublishableKey =
-    "pk_test_51H4ThcJ5SREt5PwvuHB2JXUj0MbYCwZFpZgREvJhgVD40feUTFMgLUlzEd4pnbtfkWazSCzKDv7jJno8X7HiPMcC00dXe9FS99";
+// final String baseUrl = "https://test.meinhaus.ca/api";
+// final String baseUrl2 = "https://test.meinhaus.ca";
+// final String stripePublishableKey =
+//     "pk_test_51H4ThcJ5SREt5PwvuHB2JXUj0MbYCwZFpZgREvJhgVD40feUTFMgLUlzEd4pnbtfkWazSCzKDv7jJno8X7HiPMcC00dXe9FS99";
 
 // prod env
 
-// final String baseUrl = "https://meinhaus.ca/api";
-// final String baseUrl2 = "https://meinhaus.ca";
-// final String stripePublishableKey = "pk_live_51H4ThcJ5SREt5PwvNVuthkswoxkMKololExDZy835JPptq4EyMexUtgltIaIt77Ft1m5Fs5tUgYGrnmzafRQGUl7002XHrLNsB";
+final String baseUrl = "https://meinhaus.ca/api";
+final String baseUrl2 = "https://meinhaus.ca";
+final String stripePublishableKey = "pk_live_51H4ThcJ5SREt5PwvNVuthkswoxkMKololExDZy835JPptq4EyMexUtgltIaIt77Ft1m5Fs5tUgYGrnmzafRQGUl7002XHrLNsB";
 
 
 // ======
